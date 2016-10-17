f43b913ebe49a6f8e8319ba7bf9b0e0f647e9f9a
RLopez <romariolopezc@gmail.com>
2016-10-17 09:13:13 -0500

    IPV4 parser test added

--- 2. A whirlwind tour/exercises_test.exs
+++ 2. A whirlwind tour/exercises_test.exs
@@ -20,5 +20,26 @@ defmodule Chapter2.ExercisesTest do
       <<79, 132, 235, 77, 3, 224, 121, 88, 98, 75, 61, 67, 62, 16, 233, 91>>
   end
 
-  test "ipv4 parser"
+  test "ipv4 parser" do
+    packet = <<
+     4 :: size(4), # version
+     5 :: size(4), # ihl
+     1 :: size(6), # dscp
+     1 :: size(2), # ecn
+     1 :: size(16), # total_length
+     1 :: size(16), # identification
+     1 :: size(3), # flags
+     1 :: size(13), # fragment_offset
+     1 :: size(8), # ttl
+     6 :: size(8), # protocol
+     1 :: size(16), # header_checksum
+     192 :: size(8), 168 :: size(8), 0 :: size(8), 1 :: size(8), # src_ip_address (32 bits)
+     192 :: size(8), 168 :: size(8), 0 :: size(8), 2 :: size(8), # dest_ip_address (32 bits)
+     1 :: size(8) # rest
+   >>
+   assert %{
+    source_ip: "192.168.0.1",
+    destination_ip: "192.168.0.2"
+   } == ipv4_parser(packet)
+  end
 end
